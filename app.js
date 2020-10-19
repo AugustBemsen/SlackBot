710b2d8a0dd2608fc4907a9b55e3e6759bf8ba7e
Daniel Bemsen <danireptor7@gmail.com>
2020-10-19 02:52:42 +0100

    commiting promote mentors handler

--- app.js
+++ app.js
@@ -17,21 +17,19 @@ const web = new WebClient(token);
 const userToken = process.env.USER_TOKEN;
 const testChannel = "testbot";
 const testText = `Promotion Bot ${package.version}  has started`;
-const promoteChannel = "C01CDGS324B";
-const removeChannel = "C01BY2PJ3TR";
-const promoteUsers = ["U01D2PWDY56"];
+// const promoteUsers = ["U01D2PWDY56"];
 // const usersToRemove = ["U01CD2W0QRG"];
 // Initializing bot
 
 rtm.on("ready", async () => {
-    // console.log(testText);
-    // await sendMessage("#level-2", `Promotion Bot ${package.version}  is loading....`);
-    // await setTimeout( sendMessage("#level-2", `Promotion Bot has started`), 3000);
-    // await setTimeout( sendMessage("#level-2", `<@SideHustle_Bot> is ready`), 5000);
+    console.log(`Promotion Bot ${package.version}  has started`);
     // await sendMessage("#testbot", `Promotion Bot ${package.version}  has started`);
-    // await promoteUser("G01CUFFAR27", "C01CDGS324B");
+    // await promoteUser("C01CUFFAR27", "C01CDGS324B");
     // await removeUser();
     // await deactivateUsers();
+    // isolateUsers();
+    // promoteMentors("C01CUFFAR27");
+
 })
 
 
@@ -46,23 +44,49 @@ const sendMessage = async (testChannel, testText) => {
 
 //  Add users to channel function 
 
+
 const promoteUser = async (promoteChannel, removeChannel) => {
-    // Make a request for a user with a given ID
-    await promoteUsers.map(user => {
-        axios.post(`https://slack.com/api/conversations.invite?token=${userToken}&channel=${promoteChannel}&users=${user}&pretty=1`)
-            .then((response) => {
-                console.log("add res", response.data);
+    // get users from database
+    await axios.get('https://sidehustle.ng/dashboard/json.php')
+        .then(async (res) => {
+            // Make a request for a user with a given ID
+            await res.data.map(user => {
+                console.log(user.slack_id);
+                axios.post(`https://slack.com/api/conversations.invite?token=${userToken}&channel=${promoteChannel}&users=${user.slack_id}&pretty=1`)
+                    .then((response) => {
+                        console.log("add res", response.data);
+                    })
+                    .catch((error) => {
+                        console.log("add err", error.data);
+                    });
+                axios.post(`https://slack.com/api/conversations.kick?token=${userToken}&channel=${removeChannel}&user=${user.slack_id}&pretty=1`)
+                    .then((response) => {
+                        console.log("remove res", response.data);
+                    }).catch((error) => {
+                        console.log("remove err", error.data);
+                    });
             })
-            .catch((error) => {
-                console.log("add err", error.data);
-            });
-        axios.post(`https://slack.com/api/conversations.kick?token=${userToken}&channel=${removeChannel}&user=${user}&pretty=1`)
-            .then((response) => {
-                console.log("remove res", response.data);
-            }).catch((error) => {
-                console.log("remove err", error.data);
-            });
-    })
+        }).catch(console.error())
+};
+
+//  Promote mentors
+
+const promoteMentors = async (promoteChannel) => {
+    // get users from database
+    await axios.get(`https://slack.com/api/conversations.members?token=${token}&channel=C01B1J54RRV&pretty=`)
+        .then(async (res) => {
+            // Make a request for a user with a given ID
+
+            await res.data.members.map(user => {
+                axios.post(`https://slack.com/api/conversations.invite?token=${userToken}&channel=${promoteChannel}&users=${user}&pretty=1`)
+                    .then((response) => {
+                        console.log("add res", response.data);
+                    })
+                    .catch((error) => {
+                        console.log("add err", error.data);
+                    });
+            })
+        }).catch(console.error())
 };
 
 // Deactivate Users
@@ -79,17 +103,34 @@ const deactivateUsers = async () => {
     })
 };
 
+//  isolate inactive Users
+
+const isolateUsers = async () => {
+    await axios.get(`https://slack.com/api/users.list?token=${userToken}&pretty=1`)
+        .then(async (response) => {
+            await response.data.members.map(element => {
+                if (element.is_invited_user) {
+                    console.log(response.data.members);
+                }
+            });
+
+        })
+        .catch((error) => {
+            console.log("isolate err", error);
+        });
+}
+
 // Check for events 
 
 rtm.on("slack_event", async (eventType, event) => {
     if (event && event.type === "message") {
-        console.log(event);
+        // console.log(event);
         if (event.text === "Hello Bot") {
             await sendMessage(event.channel, `Hey, how are you <@${event.user}>`);
         }
     }
 })
 
-rtm.start().catch(console.error);
+// rtm.start().catch(console.error);
 
-// rtm.disconnect().catch(console.error);
+// rtm.disconnect().catch(console.log("Bot disconnected"));
